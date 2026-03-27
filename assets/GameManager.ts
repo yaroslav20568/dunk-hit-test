f837e92b79c5f161eae1003bf7c88675910ac5d8
Yaroslav <62301460+yaroslav20568@users.noreply.github.com>
2026-03-27 09:24:28 -0400

    feat: add GameManager with screen management logic

--- assets/GameManager.ts
+++ assets/GameManager.ts
@@ -1,12 +1,35 @@
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
-export default class NewClass extends cc.Component {
-    @property(cc.Label)
-    label: cc.Label = null;
+export default class GameManager extends cc.Component {
+	@property(cc.Node)
+	startScreen: cc.Node = null;
 
-    @property
-    text: string = 'hello';
+	@property(cc.Node)
+	gameScreen: cc.Node = null;
 
-    start () {}
+	@property(cc.Node)
+	resultScreen: cc.Node = null;
+
+	onLoad() {
+		this.showStartScreen();
+	}
+
+	showStartScreen() {
+		this.startScreen.active = true;
+		this.gameScreen.active = false;
+		this.resultScreen.active = false;
+	}
+
+	showGameScreen() {
+		this.startScreen.active = false;
+		this.gameScreen.active = true;
+		this.resultScreen.active = false;
+	}
+
+	showResultScreen() {
+		this.startScreen.active = false;
+		this.gameScreen.active = false;
+		this.resultScreen.active = true;
+	}
 }
